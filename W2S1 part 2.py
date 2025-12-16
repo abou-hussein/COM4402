ba730cb0d9164da8a0da4ea20d399bd3c7f5c08f
ah33crt <aboubakarhussein00@gmail.com>
2025-12-16 14:58:37 +0000

    w2s1 class

---
+++ W2S1 part 2.py
@@ -0,0 +1,25 @@
+# activity
+#print ("Your details")
+#print ("Name: Alice")
+#print ("Age: 20")
+#print ("course: COM4402")
+
+# activity 1
+#item_name = input ("enter item name:")
+#price = float (input("Enter price:"))
+#quantity = int (input ("enter quantity:"))
+#print (F"You bought {quantity} x {item_name} at £{price*quantity} each")
+
+# activity 2
+#age = int(input("Enter your age: "))
+#print ("You are", age, "years old.")
+#print (f"You are {age} years old.")
+#print ("The f-string version is easier to read and maintain")
+
+# activity 3
+#name = input ("enter your name:")
+#age = int(input("Enter your age:"))
+#course = input ("enter your course:")
+#print (f"Your details:/nName: {name}/nAge: {age}/nCourse: {course}")
+
+# activity 4
---
+++ W2S1.py
@@ -0,0 +1,8 @@
+name =str(input("enter your name:"))
+age = int(input ("enter your age:"))
+height = float(input("enter your height in metres"))
+age_plus_one = age + 1
+print ("hello", name)
+print ("next year you will be", age_plus_one)
+print ("your height is", height, "metres")
+
