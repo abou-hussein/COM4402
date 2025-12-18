40fb41e0f7e716aa8f5ec2bd2e8a15fe6234558e
ah33crt <aboubakarhussein00@gmail.com>
2025-12-18 14:51:04 +0000

    w2s1 class

---
+++ W2S2.py
@@ -0,0 +1,70 @@
+# activity 1
+# age = int (input("enter your age"))
+# if age <= 5:
+#     print("free entry")
+# elif age <= 17:
+#     print ("Child ticket")
+# elif age <= 64:
+#     print ("adult ticket")
+# else:
+#     print ("senior ticket")
+
+# activity 2
+# days_late = int(input("enter number of days late"))
+# if days_late == 0:
+#     print("No fine")
+# elif days_late <= 5:
+#     print("Fine = 1")
+# elif days_late <= 10:
+#     print("Fine = 5")
+# else:
+#     print("Fine = 10 and membership review")
+
+# activity 3
+# score = int (input("enter your score"))
+# if score >= 40:
+#     if 38 <= score <= 42:
+#         print ("Borderline pass, consider review")
+#     else:
+#         print ("clear pass")
+# else:
+#     print ("fail")
+
+# activity 4
+# is_student = input("Are you a student? (yes/no): ").lower()
+# age = int(input("Enter your age: "))
+#
+# if is_student == "yes" or age < 18:
+#     print("Discount applied")
+# elif is_student == "no" and age >= 18:
+#     print("No")
+# else:
+#     print("No")
+
+# activity 5
+# colour = input("Enter a traffic light colour (red, amber, or green): ").lower()
+#
+# if colour == "red":
+#     print("Stop")
+# elif colour == "amber":
+#     print("Get ready")
+# elif colour == "green":
+#     print("Go")
+# else:
+#     print("Invalid colour")
+
+# activity 6
+# number = int(input("Enter a number: "))
+#
+# if number % 3 == 0 and number % 5 == 0:
+#     print("FizzBuzz")
+# elif number % 3 == 0:
+#     print("Fizz")
+# elif number % 5 == 0:
+#     print("Buzz")
+# else:
+#     print("No match")
+
+# activity 7
+
+
